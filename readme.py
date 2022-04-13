c1648757fa3c1a7196be6c2435f6f3fb660b6a1b
Peter Zhang <103570294+PeterZhang2022@users.noreply.github.com>
2022-04-13 18:22:57 +0800

    v0.1
    test

---
+++ readme.py
@@ -0,0 +1,7 @@
+echo "# PeterZhang2022" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:PeterZhang2022/PeterZhang2022.git
+git push -u origin main
